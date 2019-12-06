364697baafff007aa9a4fc4f23f86acfa7fc5b20
Yahima Hadfeg <yhadfeg@gmail.com>
2019-12-06 10:50:38 -0300

    PRuebaRama

--- RecursosInformaticos3.0/CapaAccesoDatos/Conexion.cs
+++ RecursosInformaticos3.0/CapaAccesoDatos/Conexion.cs
@@ -25,7 +25,7 @@ namespace CapaAccesoDatos
         {
             SqlConnection conexion = new SqlConnection();
             conexion.ConnectionString = @"Data Source= 192.168.6.170; Initial Catalog=BD-RECURSOSTI; User ID=sa; Password=3192yahima; MultipleActiveResultSets = True";
-            //conexion.ConnectionString = @"Data Source= DESKTOP-AN7T78K; Initial Catalog=BD-RECURSOSTI; User ID=sa; Password=chirino123; MultipleActiveResultSets = True";            
+            ///conexion.ConnectionString = @"Data Source= DESKTOP-AN7T78K; Initial Catalog=BD-RECURSOSTI; User ID=sa; Password=chirino123; MultipleActiveResultSets = True";            
             return conexion;
         }
 
